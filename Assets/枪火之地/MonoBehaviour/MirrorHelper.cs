67f60849f2bc1e9f7854b87cdbd59fcf78d7b1bb
gaoxiangwu <near510@126.com>
2021-01-16 17:59:44 +0800

    调整nav

---
+++ Assets/枪火之地/MonoBehaviour/MirrorHelper.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorHelper : MonoBehaviour
+{
+    public GameObject plane;
+    public Material mat;
+
+    void Start()
+    {
+        mat.SetVector("planeNormal", plane.transform.up);
+        mat.SetVector("planePos", plane.transform.position);
+    }
+
+    void Update()
+    {
+        mat.SetVector("planeNormal", plane.transform.up);
+        mat.SetVector("planePos", plane.transform.position);
+    }
+}
